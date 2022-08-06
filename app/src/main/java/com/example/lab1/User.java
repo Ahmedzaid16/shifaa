a7a60c47183b1054bcfb8a1330141516d5f73b1f
Ahmedzaid16 <ahmedmohamed20217e@gmail.com>
2022-08-06 21:40:09 +0200

    Ahmed zaid

---
+++ app/src/main/java/com/example/lab1/User.java
@@ -0,0 +1,39 @@
+package com.example.lab1;
+
+public class User<Button> {
+
+    private String tupeOfDrag;
+    private String saleOfDrag;
+    private Integer imgDrag;
+
+    public User(String tupeOfDrag, String saleOfDrag, Integer imgDrag) {
+        this.tupeOfDrag = tupeOfDrag;
+        this.saleOfDrag = saleOfDrag;
+        this.imgDrag = imgDrag;
+    }
+
+    public String getTupeOfDrag() {
+        return tupeOfDrag;
+    }
+
+    public void setTupeOfDrag(String tupeOfDrag) {
+        this.tupeOfDrag = tupeOfDrag;
+    }
+
+    public String getSaleOfDrag() {
+        return saleOfDrag;
+    }
+
+    public void setSaleOfDrag(String saleOfDrag) {
+        this.saleOfDrag = saleOfDrag;
+    }
+
+    public Integer getImgDrag() {
+        return imgDrag;
+    }
+
+    public void setImgDrag(Integer imgDrag) {
+        this.imgDrag = imgDrag;
+    }
+
+}
